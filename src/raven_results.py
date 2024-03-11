8758d867265172c615061352b0e59a3215a74251
Calvin Xu <cmxu@comcast.net>
2024-03-11 17:33:18 -0500

    a

--- src/raven_results.py
+++ src/raven_results.py
@@ -74,7 +74,7 @@ class RavenResultList:
         raven_res = res.raven_res
         
         if args.spec_type == InputSpecType.UAP_TARGETED:
-            print('RaVeN certified UAP accuracy: {:0.2f}  %\n'.format(raven_res.verified_proportion))
+            print('RaVeN certified UAP accuracy: {}  %\n'.format(raven_res.verified_proportion))
             if individual_res is not None:
                 deepz_res = [[] for i in range(10)]
                 for i in range(len(individual_res)):
